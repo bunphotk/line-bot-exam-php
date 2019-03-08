ad1b688491465b7063bf2b362c3ffa32555aa0f4
bunphotk <kbunphot@gmail.com>
2019-03-08 21:44:03 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -41,7 +41,7 @@ if (!is_null($events['events'])) {
 					
 				$post = json_encode($data);
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-/* 
+
 				$ch = curl_init($url);
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -51,7 +51,7 @@ if (!is_null($events['events'])) {
 				$result = curl_exec($ch);
 				curl_close($ch);
 				echo $result . "\r\n";		
-									
+/* 									
 				$textMessageBuilder = new LINE\LINEBot\MessageBuilder\TextMessageBuilder(json_encode($events));
 				$response = $bot->replyMessage($replyToken,$textMessageBuilder);   
  			*/ 
