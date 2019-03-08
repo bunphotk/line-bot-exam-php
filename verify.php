e501d2e5d5ddd482cc62772d7641c2faf0119633
bunphotk <kbunphot@gmail.com>
2019-03-08 20:20:41 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Lt6AODbxFcYOtK4gUFSaYnoxnxaHc48+yuEJuMm8VNcKvx2m6sEzvCrHqEcYD2Umi+nVB1dqb5kQeEordqqCyBoOZjdxRJtfGWSwSYemHtmT6xiSq+Ohpm4sB6/1gxYiEBzBIsvcUj5yFBqnpTUI6wdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
