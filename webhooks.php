b3479fbda4762a4ed055f5c0d64bd4bf02b8a011
bunphotk <kbunphot@gmail.com>
2019-03-09 03:04:58 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -65,7 +65,8 @@ if (!is_null($events['events'])) {
 	    file_put_contents($fileName,$dataBinary); // Save file to local host
 	    
 	   // ===== Change your webservice URL for forwarding the file to your URL=====
-
+		
+$url="http://mkss.co.th/fotk/rxfile.php";	
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_VERBOSE, 0);
@@ -73,8 +74,7 @@ curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
 $post = array(
-    'rxfile'=> "@$fileName"
-    
+    'rxfile'=> "@$fileName"    
 );
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 $response = curl_exec($ch);
